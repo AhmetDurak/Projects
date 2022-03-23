1ce5e8b39f00cc6b7455b253f2d192307b8bff2a
aDrk51 <durakahmet049@gmail.com>
2022-03-24 00:40:40 +0100

    myProjects

--- src/IsPasswordStrong.java
+++ src/IsPasswordStrong.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class IsPasswordStrong {
     public static void main(String[] args) {
 
+        System.out.println(new IsPasswordStrong().checkPassword(new Scanner(System.in).nextLine()));
+
+    }
+
+    boolean checkPassword(String password) {
+        boolean hasUpperCase = false, hasLowerCase = false, hasSpecialChar = false;
+        boolean hasNumber = false;
+
+        boolean hasEnoughLength = password.length() >= 8 && !password.contains(" ");
+
+        for (char everyLetter: password.toCharArray()){
+            if (Character.isUpperCase(everyLetter)) hasUpperCase = true;
+            if (Character.isLowerCase(everyLetter)) hasLowerCase = true;
+            if (Character.isDigit(everyLetter)) hasNumber = true;
+            if (!Character.isLetterOrDigit(everyLetter)) hasSpecialChar = true;
+        }
+
+        return hasEnoughLength && hasUpperCase && hasLowerCase&& hasNumber && hasSpecialChar;
     }
 
 
 }
+
+
+// password length should be more than 8
+// special character
+// number and Uppercase
+//
